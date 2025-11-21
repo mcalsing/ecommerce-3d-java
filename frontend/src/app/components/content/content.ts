45f28d27131596670e280ea209eb8690d1523de8
mcalsing <marcelocalsing@gmail.com>
2025-11-21 20:34:28 -0300

    feat(frontend): adicionado hero imagena home page e alterado cores do site

--- frontend/src/app/components/content/content.ts
+++ frontend/src/app/components/content/content.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { BaseCard } from '../base-card/base-card';
 import { ShadeCard } from '../shade-card/shade-card';
+import { Hero } from '../hero/hero';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [BaseCard, ShadeCard],
+  imports: [BaseCard, ShadeCard, Hero],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
