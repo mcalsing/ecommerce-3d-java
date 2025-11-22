9b40f578cef0e4d582bf7f8396551775d51595ad
mcalsing <marcelocalsing@gmail.com>
2025-11-21 22:18:12 -0300

    feat(frontend): paginas de login e registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Content } from './components/content/content';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Content },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register }
+];
