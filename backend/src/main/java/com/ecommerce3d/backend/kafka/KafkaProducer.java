07576c5dd24fd64d06743f4b8f1fa8909af2d4b8
mcalsing <marcelocalsing@gmail.com>
2025-11-24 15:26:34 -0300

    feat(backend): kafka funcionando, está recebendo msg do pedido

--- backend/src/main/java/com/ecommerce3d/backend/kafka/KafkaProducer.java
+++ backend/src/main/java/com/ecommerce3d/backend/kafka/KafkaProducer.java
@@ -1,18 +1,30 @@
 package com.ecommerce3d.backend.kafka;
 
 import com.ecommerce3d.backend.dtos.OrderKafkaDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaProducer {
-  private final KafkaTemplate<String, OrderKafkaDTO> kafkaTemplate;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  private final ObjectMapper objectMapper;
 
-  public KafkaProducer(KafkaTemplate<String, OrderKafkaDTO> kafkaTemplate) {
+  public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
     this.kafkaTemplate = kafkaTemplate;
+    this.objectMapper = objectMapper;
   }
 
   public void send(OrderKafkaDTO dto) {
-    kafkaTemplate.send("order-print", dto);
+    try {
+      // Converte o DTO para JSON
+      String json = objectMapper.writeValueAsString(dto);
+
+      kafkaTemplate.send("orders-topic", json);
+      System.out.println("📤 Enviado para Kafka: " + json);
+
+    } catch (Exception e) {
+      throw new RuntimeException("Erro ao serializar OrderKafkaDTO", e);
+    }
   }
 }
